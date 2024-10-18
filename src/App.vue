a6d6b96499bdf3bffbfee1cc630fe51992c3a7b9
ckjr3000 <charlotte.kj.roe@gmail.com>
2024-10-18 15:16:19 +0100

    Set up vue project

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <label for="select-src">Add new sound source:</label>
+  <select name="select-src" value="oscillator" @change="updateSrcType">
+      <option value="oscillator">oscillator</option>
+      <option value="white noise">white noise</option>
+  </select>
+  <button @click="handleAddSoundSrc">add</button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    
+  },
+  data(){
+    return {
+      newSoundSrcType: "oscillator",
+    }
+  },
+  methods: {
+    updateSrcType(e: Event): void{
+      // Type assertion and null check for target
+      const selectElement = e.target as HTMLSelectElement | null;
+
+      if (selectElement) {
+        this.newSoundSrcType = selectElement.value;
+      } else {
+        console.error("No target element found for the event.");
+      }
+    },
+    handleAddSoundSrc(): void{
+      console.log(this.newSoundSrcType);
+    },
   }
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
